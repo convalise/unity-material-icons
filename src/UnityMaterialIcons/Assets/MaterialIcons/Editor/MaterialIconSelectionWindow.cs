f1783bc66e7d2ef474c5115e82a022dc50fec627
convalise <convalise@gmail.com>
2021-03-05 21:00:26 -0300

    Greatly improved the selection window.

--- src/UnityMaterialIcons/Assets/MaterialIcons/Editor/MaterialIconSelectionWindow.cs
+++ src/UnityMaterialIcons/Assets/MaterialIcons/Editor/MaterialIconSelectionWindow.cs
@@ -13,21 +13,23 @@ public class MaterialIconSelectionWindow : EditorWindow
 {
 	private static readonly Color darkColor = new Color(0.196f, 0.196f, 0.196f);
 	private static readonly Color lightColor = new Color(0.804f, 0.804f, 0.804f);
-	
+
 	private int iconSize = 60;
-	private int iconZoom = 1;
+	private int labelHeight = 24;
+	private int spacing = 10;
 	private bool showNames = true;
 	private Vector2 scrollPos = Vector2.zero;
 	private string filterText = string.Empty;
 	private bool filterGotFocus = false;
 
 	private string selected;
-	private int selectedIndex;
+	private string selectedName;
 	private bool selectionKeep;
-	private System.Action<string> OnSelectionChanged;
+	private System.Action<string> onSelectionChanged;
 
 	private Font MaterialIconsRegular;
-	private CodepointData[] CodepointsCollection;
+	private CodepointData[] codepointsCollection;
+	private CodepointData[] filteredCollection;
 
 	private GUIStyle toolbarSeachTextFieldStyle;
 	private GUIStyle toolbarSeachCancelButtonStyle;
@@ -39,17 +41,16 @@ public class MaterialIconSelectionWindow : EditorWindow
 
 	public void LoadDependencies(Font MaterialIconsRegular)
 	{
-		showNames = EditorPrefs.GetBool("MaterialIconSelectionWindow.showNames", true);
-		iconZoom = EditorPrefs.GetInt("MaterialIconSelectionWindow.iconZoom", 1);
+		showNames = EditorPrefs.GetBool(typeof(MaterialIconSelectionWindow) + ".showNames", true);
 
 		if(MaterialIconsRegular == null)
 			return;
-		
+
 		this.MaterialIconsRegular = MaterialIconsRegular;
 
 		string fontPath = AssetDatabase.GetAssetPath(MaterialIconsRegular);
 		string codepointsPath = Path.GetDirectoryName(fontPath) + "/codepoints";
-	
+
 		List<CodepointData> tempList = new List<CodepointData>();
 
 		foreach(string codepoint in File.ReadAllLines(codepointsPath))
@@ -58,22 +59,26 @@ public class MaterialIconSelectionWindow : EditorWindow
 			tempList.Add(new CodepointData(data[0], data[1]));
 		}
 
-		CodepointsCollection = tempList.ToArray();
-		selectedIndex = System.Array.FindIndex(CodepointsCollection, (data) => data.codeGUIContent.text == selected);
+		codepointsCollection = tempList.ToArray();
+		filteredCollection = codepointsCollection;
+
+		var temp = filteredCollection.FirstOrDefault(data => data.codeGUIContent.text == selected);
+		if(temp != null)
+			selectedName = temp.name;
 	}
 
 	public static void Init(Font MaterialIconsRegular, string preSelect, System.Action<string> callback)
 	{
 		MaterialIconSelectionWindow window = EditorWindow.GetWindow<MaterialIconSelectionWindow>(true);
 		window.selected = preSelect;
-		window.OnSelectionChanged = callback;
+		window.onSelectionChanged = callback;
 		window.LoadDependencies(MaterialIconsRegular);
 	}
 
 	private void OnEnable()
 	{
 		base.titleContent = new GUIContent("Material Icon Selection");
-		base.minSize = new Vector2(430f, 570f);
+		base.minSize = new Vector2((iconSize + labelHeight + spacing) * 5f + GUI.skin.verticalScrollbar.fixedWidth + 1f, (iconSize + labelHeight + spacing) * 6f + EditorStyles.toolbar.fixedHeight);
 		selectionKeep = true;
 	}
 
@@ -85,13 +90,12 @@ public class MaterialIconSelectionWindow : EditorWindow
 			toolbarSeachCancelButtonStyle = new GUIStyle("ToolbarSeachCancelButton");
 			toolbarSeachCancelButtonEmptyStyle = new GUIStyle("ToolbarSeachCancelButtonEmpty");
 			toolbarLabelStyle = new GUIStyle(EditorStyles.label) { alignment = TextAnchor.MiddleCenter };
-			iconImageStyle = new GUIStyle() { font = MaterialIconsRegular, alignment = TextAnchor.MiddleCenter };
+			iconSelectionStyle = new GUIStyle("selectionrect");
+			iconImageStyle = new GUIStyle() { font = MaterialIconsRegular, fontSize = iconSize - spacing, alignment = TextAnchor.MiddleCenter };
 			iconLabelStyle = new GUIStyle(EditorStyles.miniLabel) { alignment = TextAnchor.UpperCenter, wordWrap = true };
-			iconSelectionStyle = new GUIStyle("RL Element");
+			iconImageStyle.padding = iconLabelStyle.padding = new RectOffset();
 			iconImageStyle.normal.textColor = iconLabelStyle.normal.textColor = EditorGUIUtility.isProSkin ? lightColor : darkColor;
 		}
-
-		OnHeaderGUI();
 
 		if(MaterialIconsRegular == null)
 		{
@@ -99,26 +103,34 @@ public class MaterialIconSelectionWindow : EditorWindow
 			return;
 		}
 
-		if((CodepointsCollection == null) || (CodepointsCollection.Length == 0))
+		if((codepointsCollection == null) || (codepointsCollection.Length == 0))
 		{
 			EditorGUILayout.HelpBox("Could not find \"codepoints\" font data.", MessageType.Error);
 			return;
 		}
 
+		OnHeaderGUI();
 		OnBodyGUI();
 	}
 
 	private void OnHeaderGUI()
 	{
-		EditorGUILayout.BeginHorizontal(EditorStyles.toolbar, GUILayout.ExpandWidth(true));
+		Rect groupRect = new Rect(0f, 0f, base.position.width, EditorStyles.toolbar.fixedHeight);
+		GUI.BeginGroup(groupRect);
 
-		GUI.SetNextControlName("MaterialIconSelectionWindow.filterText");
-		filterText = EditorGUILayout.TextField(filterText, toolbarSeachTextFieldStyle);
+		if(Event.current.type == EventType.Repaint)
+		{
+			EditorStyles.toolbar.Draw(groupRect, false, false, false, false);
+		}
 
-		Rect clearRect = GUILayoutUtility.GetRect(20f, 14f, GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false));
-		clearRect.y += 1.5f;
+		Rect filterRect = new Rect(6f, 2f, groupRect.width - 6f - 20f - 64f - 6f, groupRect.height - 2f);
+		Rect clearRect = new Rect(filterRect.x + filterRect.width, filterRect.y, 20f, filterRect.height);
 
-		GUI.Label(clearRect, GUIContent.none, toolbarSeachCancelButtonEmptyStyle);
+		EditorGUI.BeginChangeCheck();
+
+		GUI.SetNextControlName(typeof(MaterialIconSelectionWindow) + ".filterText");
+		filterText = EditorGUI.TextField(filterRect, filterText, toolbarSeachTextFieldStyle);
+
 		if(GUI.Button(clearRect, GUIContent.none, string.IsNullOrEmpty(filterText) ? toolbarSeachCancelButtonEmptyStyle : toolbarSeachCancelButtonStyle))
 		{
 			filterText = string.Empty;
@@ -127,108 +139,169 @@ public class MaterialIconSelectionWindow : EditorWindow
 
 		if(!filterGotFocus)
 		{
-			EditorGUI.FocusTextInControl("MaterialIconSelectionWindow.filterText");
+			EditorGUI.FocusTextInControl(typeof(MaterialIconSelectionWindow) + ".filterText");
 			filterGotFocus = true;
 		}
 
-		Rect nameRect = GUILayoutUtility.GetRect(64f, 14f, GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false));
+		if(EditorGUI.EndChangeCheck())
+		{
+			filteredCollection = codepointsCollection.Where(data => string.IsNullOrEmpty(filterText) || data.nameGUIContent.text.IndexOf(filterText, System.StringComparison.OrdinalIgnoreCase) >= 0).ToArray();
+			selectionKeep = true;
+		}
+
+		Rect nameRect = new Rect(clearRect.x + clearRect.width, groupRect.y, 64f, groupRect.height);
 		EditorGUI.BeginChangeCheck();
 		showNames = EditorGUI.Toggle(nameRect, showNames, EditorStyles.toolbarButton);
 		if(EditorGUI.EndChangeCheck())
 		{
-			EditorPrefs.SetBool("MaterialIconSelectionWindow.showNames", showNames);
+			GUI.FocusControl(null);
+			EditorPrefs.SetBool(typeof(MaterialIconSelectionWindow) + ".showNames", showNames);
 		}
-		nameRect.y += 1.5f;
 		EditorGUI.LabelField(nameRect, "Names", toolbarLabelStyle);
 
-		Rect zoomRect = GUILayoutUtility.GetRect(32f, 14f, GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false));
-		EditorGUI.BeginChangeCheck();
-		EditorGUI.Toggle(zoomRect, iconZoom == 2, EditorStyles.toolbarButton);
-		if(EditorGUI.EndChangeCheck())
-		{
-			iconZoom = (iconZoom == 1) ? 2 : 1;
-			EditorPrefs.SetInt("MaterialIconSelectionWindow.iconZoom", iconZoom);
-		}
-		zoomRect.y += 1.5f;
-		EditorGUI.LabelField(zoomRect, "x2", toolbarLabelStyle);
-
-		iconImageStyle.fontSize = Mathf.FloorToInt((iconSize - 10) * iconZoom);
-
-		EditorGUILayout.EndHorizontal();
+		GUI.EndGroup();
 	}
 
 	private void OnBodyGUI()
 	{
-		scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
-
-		EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(false));
-		GUILayout.Space(showNames ? 4f : 0f);
-
-		int k = 0;
-		int c = Mathf.FloorToInt((base.position.width - 16f) / ((iconSize * iconZoom) + 8f));
-		for(int i = 0; i < CodepointsCollection.Length; i++)
+		Rect iconRect = new Rect(0f, 0f, iconSize + labelHeight, iconSize);
+		Rect labelRect = new Rect(0f, 0f, iconRect.width, labelHeight);
+		if(!showNames)
 		{
-			var data = CodepointsCollection[i];
+			iconRect.width -= labelHeight;
+			labelRect.height = 0f;
+		}
+		Rect buttonRect = new Rect(0f, 0f, iconRect.width + spacing, iconRect.height + labelRect.height + spacing);
 
-			if(!string.IsNullOrEmpty(filterText) && filterText.Split(' ').Any(filter => data.nameGUIContent.text.IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) < 0))
-				continue;
+		Rect groupRect = new Rect(0f, EditorStyles.toolbar.fixedHeight, base.position.width, base.position.height - EditorStyles.toolbar.fixedHeight);
+		GUI.BeginGroup(groupRect);
 
-			if(k++ % c == 0)
+		Rect scrollRect = new Rect(0f, 0f, groupRect.width, groupRect.height);
+		int columns = Mathf.FloorToInt((scrollRect.width - GUI.skin.verticalScrollbar.fixedWidth) / (iconRect.width + spacing));
+		Rect viewRect = new Rect(0f, 0f, scrollRect.width - GUI.skin.verticalScrollbar.fixedWidth, Mathf.Ceil(filteredCollection.Length / (float) columns) * (iconRect.height + labelRect.height + spacing));
+
+		scrollPos = GUI.BeginScrollView(scrollRect, scrollPos, viewRect);
+
+		for(int i = 0; i < filteredCollection.Length; i += columns)
+		{
+			for(int j = 0; j < columns; j++)
 			{
-				GUILayout.FlexibleSpace();
-				EditorGUILayout.EndHorizontal();
-				EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(false));
-				GUILayout.Space(showNames ? 4f : 0f);
-			}
+				if((i + j) >= filteredCollection.Length)
+					break;
 
-			GUILayout.Space(showNames ? 4f : 8f);
-			EditorGUILayout.BeginVertical();
-			GUILayout.Space(4f);
+				var data = filteredCollection[i + j];
 
-			Rect irect = GUILayoutUtility.GetRect(iconSize * iconZoom, iconSize * iconZoom, GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false));
-			Rect lrect = Rect.zero;
-			if(showNames)
-			{
-				lrect = GUILayoutUtility.GetRect(iconSize * iconZoom - 4f, iconLabelStyle.CalcHeight(data.nameGUIContent, iconSize * iconZoom - 4f), iconLabelStyle, GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(false));
-				lrect.x = irect.x;
-				lrect.width = irect.width;
-			}
-			Rect brect = new Rect(irect) { height = irect.height + lrect.height };
-			Rect hrect = new Rect(brect.x - 5, brect.y - 5, brect.width + 10, brect.height + 10);
+				iconRect.x = (j * (iconRect.width + spacing)) + (spacing / 2f);
+				iconRect.y = ((i / (float) columns) * (iconRect.height + labelRect.height + spacing)) + (spacing / 2f);
 
-			if(Event.current.type == EventType.Repaint)
-			{
-				if(i == selectedIndex)
+				labelRect.x = iconRect.x;
+				labelRect.y = iconRect.y + iconRect.height;
+
+				buttonRect.x = iconRect.x - (spacing / 2f);
+				buttonRect.y = iconRect.y - (spacing / 2f);
+
+				if(data.name == selectedName)
 				{
-					iconSelectionStyle.Draw(hrect, false, true, true, true);
+					if(Event.current.type == EventType.Repaint)
+					{
+						iconSelectionStyle.Draw(buttonRect, false, true, true, true);
+					}
+
 					if(selectionKeep)
 					{
-						scrollPos.y = Mathf.Clamp(scrollPos.y, irect.y + irect.height + lrect.height - base.position.height + EditorStyles.toolbar.fixedHeight, irect.y);
+						if(buttonRect.y + buttonRect.height > scrollPos.y + scrollRect.height)
+							scrollPos.y = buttonRect.y + buttonRect.height - scrollRect.height;
+						else if(buttonRect.y < scrollPos.y)
+							scrollPos.y = buttonRect.y;
+
 						selectionKeep = false;
 						base.Repaint();
 					}
 				}
+
+				GUI.Label(iconRect, data.codeGUIContent, iconImageStyle);
+				if(showNames)
+					GUI.Label(labelRect, data.nameGUIContent, iconLabelStyle);
+
+				if(GUI.Button(buttonRect, GUIContent.none, GUIStyle.none))
+				{
+					GUI.FocusControl(null);
+					selected = data.codeGUIContent.text;
+					selectedName = data.name;
+					onSelectionChanged.Invoke(selected);
+				}
 			}
-
-			EditorGUILayout.EndVertical();
-
-			GUI.Label(irect, data.codeGUIContent, iconImageStyle);
-			if(showNames)
-				GUI.Label(lrect, data.nameGUIContent, iconLabelStyle);
-
-			if(GUI.Button(brect, GUIContent.none, GUIStyle.none))
-			{
-				selected = data.codeGUIContent.text;
-				selectedIndex = i;
-				OnSelectionChanged.Invoke(selected);
-			}
-
 		}
 
-		GUILayout.FlexibleSpace();
-		EditorGUILayout.EndHorizontal();
+		GUI.EndScrollView();
+		GUI.EndGroup();
 
-		GUILayout.EndScrollView();
+		if(Event.current.type == EventType.KeyDown)
+		{
+			if(Event.current.keyCode == KeyCode.LeftArrow)
+			{
+				SelectRelative(-1);
+				Event.current.Use();
+			}
+
+			if(Event.current.keyCode == KeyCode.RightArrow)
+			{
+				SelectRelative(+1);
+				Event.current.Use();
+			}
+
+			if(Event.current.keyCode == KeyCode.UpArrow)
+			{
+				SelectRelative(-columns);
+				Event.current.Use();
+			}
+
+			if(Event.current.keyCode == KeyCode.DownArrow)
+			{
+				SelectRelative(+columns);
+				Event.current.Use();
+			}
+
+			if(Event.current.keyCode == KeyCode.PageUp)
+			{
+				SelectRelative(-(columns * 6));
+				Event.current.Use();
+			}
+
+			if(Event.current.keyCode == KeyCode.PageDown)
+			{
+				SelectRelative(+(columns * 6));
+				Event.current.Use();
+			}
+
+			if(Event.current.keyCode == KeyCode.Home)
+			{
+				SelectAbsolute(0);
+				Event.current.Use();
+			}
+
+			if(Event.current.keyCode == KeyCode.End)
+			{
+				SelectAbsolute(filteredCollection.Length - 1);
+				Event.current.Use();
+			}
+		}
+	}
+
+	private void SelectRelative(int delta)
+	{
+		SelectAbsolute(System.Array.FindIndex(filteredCollection, (data) => data.name == selectedName) + delta);
+	}
+
+	private void SelectAbsolute(int index)
+	{
+		index = Mathf.Clamp(index, 0, filteredCollection.Length - 1);
+
+		selected = filteredCollection[index].codeGUIContent.text;
+		selectedName = filteredCollection[index].name;
+		onSelectionChanged.Invoke(selected);
+		selectionKeep = true;
+		base.Repaint();
 	}
 
 	[System.Serializable]
